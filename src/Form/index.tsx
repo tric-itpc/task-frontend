5bfd84e42711b3e553f293d3854f19a9721b1b54
Kristina Andreeva <tabbyk@ya.ru>
2021-11-11 21:52:15 +0300

    add yup validation

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 import FormField from "../components/FormField";
 import { EMessageCategory } from "../types";
@@ -10,7 +11,7 @@ interface IFormValues {
   firstName: string;
   lastName: string;
   email: string;
-  category: EMessageCategory | null;
+  category: EMessageCategory | "";
   message: string;
   image?: string;
 }
@@ -21,11 +22,37 @@ const categories = [
   { value: EMessageCategory.Third, label: "Третья" },
 ];
 
+Yup.setLocale({
+  mixed: {
+    required: "Обязательное поле.",
+  },
+});
+
+const validationSchema = Yup.object().shape(
+  {
+    firstName: Yup.string().when("lastName", {
+      is: (value: string) => !value,
+      then: Yup.string().required("Введите имя и/или фамилию."),
+      otherwise: Yup.string(),
+    }),
+    lastName: Yup.string().when("firstName", {
+      is: (value: string) => !value,
+      then: Yup.string().required("Введите имя и/или фамилию."),
+      otherwise: Yup.string(),
+    }),
+    email: Yup.string().email("Введите валидный email.").required(),
+    category: Yup.string().required(),
+    message: Yup.string().min(10, "Минимум 10 символов.").required(),
+  },
+  // noSortEdges?: Array<[string, string]>
+  [["firstName", "lastName"]]
+);
+
 const initialValues: IFormValues = {
   firstName: "",
   lastName: "",
   email: "",
-  category: null,
+  category: "",
   message: "",
 };
 
@@ -37,16 +64,20 @@ const MyForm: React.FC = () => {
   return (
     <div className={styles.root}>
       <h2>Форма обратной связи</h2>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        <Form>
-          <FormField label='Имя' name='firstName' type='text' />
-          <FormField label='Фамилия' name='lastName' type='text' />
-          <FormField label='E-mail' name='email' type='email' />
-          <FormField label='Категория' name='category' component='select' options={categories} />
-          <FormField label='Сообщение' name='message' component='textarea' />
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+        {({ isValid, dirty }) => (
+          <Form>
+            <FormField label='Имя' name='firstName' type='text' />
+            <FormField label='Фамилия' name='lastName' type='text' />
+            <FormField label='E-mail' name='email' type='email' />
+            <FormField label='Категория' name='category' component='select' options={categories} />
+            <FormField label='Сообщение' name='message' component='textarea' />
 
-          <Button type='submit'>Отправить</Button>
-        </Form>
+            <Button disabled={!(isValid || dirty)} type='submit'>
+              Отправить
+            </Button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
