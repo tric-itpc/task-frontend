e66f3d7b4b839dd38721d1b96e1ac872642cf8e9
sdntmn <sdntmn@mail.ru>
2022-09-09 05:33:35 +0500

    Чистка
    Убрал не используемый useEffect

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { useValidate } from './useValidation';
 
 export function useForm(initialValue, validations) {
