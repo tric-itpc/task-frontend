0526a618e521ba116cdc0e83a9425a581626664d
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2023-04-12 14:46:15 +0500

    fix: fix variables & interface

--- frontend/src/types/Feedback.interface.ts
+++ frontend/src/types/Feedback.interface.ts
@@ -1,16 +1,15 @@
 export interface Feedback {
-  firstName: string;
-  lastName: string;
+  firstName?: string;
+  lastName?: string;
   email: string;
   category: string;
   message: string;
   image: FileList;
 }
 
-/* 
-email - обязательное поле
-имя, фамилия - должно быть заполнено одно из двух
-категория - выпадающий список. Поле обязательное, но значение по умолчанию должно быть пустым. Пустое значение не принимается
-сообщение - обязательное, минимум 10 символов
-картинка - формат jpg, png. Размер не более 2Мб
-*/
+export type FeedbackFields =
+  | 'firstName'
+  | 'lastName'
+  | 'image'
+  | 'email'
+  | 'category';
